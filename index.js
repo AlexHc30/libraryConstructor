9d301d237cf7f944e51e773ddae6512197767578
Alex Haiduc <alex.haiduc01@gmail.com>
2023-06-23 15:57:33 +0300

    Modify function and clean up code. Use for in to loop through object keys and create dom elements.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let myLibrary = [];
 
-function Book(title,author,pages,read) {
+function Book(index,title,author,pages,read) {
+    this.index = index,
     this.title = title,
     this.author = author,
     this.pages = pages,
@@ -12,53 +13,43 @@ function Book(title,author,pages,read) {
 // Write a function that loops through the array and displays each book on the page. You can display them in some sort of table, or each on their own “card”. It might help for now to manually add a few books to your array so you can see the display.
 
 function addNDisplayBooks() {
-    let index = 0;
-    let currentBook = null;
+    let currentBookTitle = null;
     let currentCategory = null;
     const library = document.createElement('div');
     library.setAttribute('class', 'library');
-    document.body.insertBefore(library, currentBook);
+    document.body.insertBefore(library, currentBookTitle);
 
 
-    // create categories for each object key and display them
+    // create categories for each object key
     let categories = Object.keys(myLibrary[0]);
     console.log(categories);
     categories.forEach(category => {
         currentCategory = category;
         currentCategory = document.createElement('div');
         currentCategory.setAttribute('id', category);
-        currentCategory.setAttribute('class', 'categories');
+        currentCategory.setAttribute('class', 'categories col');
         currentCategory.innerText = category;
         library.appendChild(currentCategory);
+        console.log(category)
+        myLibrary.map(book => {      
+            // use for(key in obj) to loop through each key, match with current category add & display
+            for(key in book) {
+                if(key === category) {
+                    let currentBookCategory = book[key];
+                    let current = document.createElement('div');
+                    current.setAttribute('id', currentBookCategory);
+                    current.setAttribute('class', 'row');
+                    current.innerText = book[key];
+                    currentCategory.appendChild(current);
+                }
+            }
+        })
     });
-
-
-    //use map to loop through each book from array
-    myLibrary.map(book => {      
-        let bookInfo = book.title + ' by ' + book.author;
-        // add new DOM element for each book & display each book
-        currentBook = document.createElement('div');
-        currentBook.setAttribute('id', "Book" + index);
-        currentBook.setAttribute('class', "books");
-        currentBook.innerText = bookInfo;
-        index++;
-        library.appendChild(currentBook);
-    })
-
-    displayBooks();
-}
-
-function displayBooks() {
-    myLibrary.map
-    let book = Object.values(myLibrary[0]);
-    console.log(book);
-    console.log(myLibrary[0]);
 }
 
 
-const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295', 'not read yet');
-// console.log(theHobbit.info());
-const unbearableLightness = new Book('The Unbearable Lightness of Being', 'JMilan Kundera', '320', 'read');
-const Gateway = new Book('Gateway', 'Frederik Pohl', '278', 'read');
+const theHobbit = new Book('1', 'The Hobbit', 'J.R.R. Tolkien', '295', 'not read yet');
+const unbearableLightness = new Book('2', 'Metamorphosis', 'Franz Kafka', '70', 'read');
+const Gateway = new Book('3', 'Gateway', 'Frederik Pohl', '278', 'read');
 
 addNDisplayBooks();
